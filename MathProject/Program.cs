27a5e418811cd2b60761b4f4545c944739c1e5c2
MartinSvab <martin.svab@centrum.cz>
2022-09-20 22:34:18 +0200

    Revamped code. Is now sleeker, more compact, and does half the stuff it used to

--- MathProject/Program.cs
+++ MathProject/Program.cs
@@ -4,39 +4,34 @@
     {
         static void Main(string[] args)
         {
-            string[] items = {"Toothpaste", "balls"};
+            string[] items = { "Toothpaste", "balls" };
             bool done = false;
             int itemNumber;
-            int itemAmount;
             string itemNumberB;
-            string itemAmountB;
-            bool itemIsNumber = false;
-            
+
 
             foreach (string item in items)
             {
                 Console.WriteLine(item);
             }
 
-
-            while (done == false)
+            while (!done)
             {
-                Console.Write("Enter number of item you wish to buy: ");
-                itemNumberB = Console.ReadLine();
-                while (itemIsNumber == false)
-                    if (itemNumberB != "1" || itemNumberB != "2" || itemNumberB != "3" || itemNumberB != "4" || itemNumberB != "5" || itemNumberB != "6" || itemNumberB != "7" || itemNumberB != "8" || itemNumberB != "9" || itemNumberB != "10")
-                    {
-                        Console.WriteLine("An item not on the selection list has been chosen, please try again: ");
-                        itemNumberB = Console.ReadLine();
-                    }
-                    else
-                    {
-                        itemIsNumber = true;    
-                    }
-                    
-                Console.Write("Enter number of times you wish to buy specified item: ");
-                Console.ReadLine();
+
+                //Entering Number Of Item
+                itemNumber = 0;
+                if (itemNumber < 1 || itemNumber > items.Length) 
+                {
+                    Console.Write("Enter the number of the item you wish to buy: ");
+                    itemNumberB = Console.ReadLine();
+                    int.TryParse(itemNumberB, out itemNumber);
+                }
+
+                //Entering Number Of Items
+
+
             }
+
         }
     }
 }
